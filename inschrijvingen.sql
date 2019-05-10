8d8a6597e894e24a552d1ebd7b707ac6434068bb
Danique Goudkuil <daniquestylinson@gmail.com>
2019-05-10 15:12:44 +0200

    Update inschrijvingen.sql

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,6 +1,6 @@
 use `lo8e_sql2`;
-INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum)
-VALUES (95248, 'Golbach', '', 'Sam', '1996-11-27', '1435 KB', 'Rijsenhout', 60, 1.73, '2019-03-04'),
-		(93183, 'Brock', '', 'Colby', '1997-01-02', '2407 BN', 'Alphen aan den Rijn', 72, 1.80, '2019-02-25'),
-        (92817, 'Centineo', '', 'Noah', '1996-05-09', '1186 WB', 'Amstelveen', 74, 1.85, '2019-01-09'),
-        (98251, 'Reinhart', '', 'Lili', '1996-09-13', '1433 BM', 'Kudelstaart', 55, 1.68, '2019-04-01');
+INSERT INTO klassen_studenten(ov_nummer, klas_code)
+VALUES (92817, 'lo9e-amo1'),
+		(95248, 'lo9e-amo2'),
+        (93183, 'lo9e-amo2'),
+        (98251, 'lo9e-amo3');

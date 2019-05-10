d53fc1cf5be1cf420f80546185167bc740f33830
Danique Goudkuil <daniquestylinson@gmail.com>
2019-05-10 15:42:43 +0200

    Update inschrijvingen.sql

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,5 +1,10 @@
 use `lo8e_sql2`;
-UPDATE studenten
-SET actief = 'inactief'
-WHERE inschrijvings_datum = '2017-01-01';
+SELECT studenten.ov_nummer, studenten.voornaam, studenten.tussenvoegsel, studenten.achternaam, klassen_studenten.klas_code, klassen.cohort, docenten.voorletters, docenten.tussenvoegsel,
+		docenten.achternaam
+FROM studenten
+LEFT JOIN klassen_studenten ON klassen_studenten.ov_nummer = studenten.ov_nummer
+LEFT JOIN klassen ON klassen.klas_code = klassen_studenten.klas_code
+LEFT JOIN docenten ON docenten.docent_code = klassen.slb_code
+WHERE NOT klassen.cohort = 2017
+ORDER BY studenten.achternaam;
 
